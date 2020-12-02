9742e9457c9050b674ffa398101ec0f58d3dd0b2
andresleon2218 <andresleon643@gmail.com>
2020-12-02 18:26:34 -0500

    Menu Bootstrap

--- DateManager/resources/views/layouts/app.blade.php
+++ DateManager/resources/views/layouts/app.blade.php
@@ -1,83 +1,178 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<!-- Meta, title, CSS, favicons, etc. -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edg  e">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+		<title>Gestor de Citas! | </title>
+		<!-- Bootstrap -->
+		<link href="{{asset('assets/dashboard/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+		<!-- Font Awesome -->
+		<link href="{{asset('assets/dashboard/vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+		<!-- NProgress -->
+		<link href="{{asset('assets/dashboard/vendors/nprogress/nprogress')}}" rel="stylesheet">
+		
+		<!-- Custom Theme Style -->
+		<link href="{{asset('assets/dashboard/build/css/custom.min.css')}}" rel="stylesheet">
+	  </head>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+	  <body class="nav-md">
+		<div class="container body">
+		  <div class="main_container">
+			<div class="col-md-3 left_col">
+			  <div class="left_col scroll-view">
+				<!-- menu profile quick info -->
+				<div class="profile clearfix">
+				  <div class="profile_pic">
+					<img src="{{asset('assets/dashboard/images/img.jpg')}}" alt="..." class="img-circle profile_img">
+				  </div>
+				  <div class="profile_info">
+					<span>Bienvenido,</span>
+					<h2>Andres Leon</h2>
+				  </div>
+				</div>
+				<!-- /menu profile quick info -->
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+				<br />
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+				<!-- sidebar menu -->
+				<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+				  <div class="menu_section">
+					<h3>General</h3>
+					<ul class="nav side-menu">
+					  <li><a><i class="fa fa-home"></i> Principal <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu">
+						  <li><a href="index.html">Opcion 1</a></li>
+						  <li><a href="index2.html">Opcion 2</a></li>						  
+						</ul>
+					  </li>
+					  <li><a><i class="fa fa-edit"></i>Profesionales <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu">
+						  <li><a href="#">Opciones</a></li>
+						 
+						</ul>
+					  </li>
+					  <li><a><i class="fa fa-desktop"></i> Usarios <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu">
+						  <li><a href="general_elements.html">Opcion 1</a></li>
+						  <li><a href="media_gallery.html">Opcion 2</a></li>
+						 
+						</ul>
+					  </li>
+					  <li><a><i class="fa fa-table"></i> Reportes <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu">
+						  <li><a href="#">Reporte 1</a></li>
+						  <li><a href="#">Reporte 2</a></li>
+						</ul>
+					  </li>
+					  <li><a><i class="fa fa-bar-chart-o"></i> Calendario <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu">
+						  <li><a href="chartjs.html">Opciones</a></li>
+						 
+						</ul>
+					  </li>					 
+					</ul>
+				  </div>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+				</div>
+				<!-- /sidebar menu -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+				<!-- /menu footer buttons -->
+				<div class="sidebar-footer hidden-small">
+				  <a data-toggle="tooltip" data-placement="top" title="Settings">
+					<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+				  </a>
+				  <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+					<span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+				  </a>
+				  <a data-toggle="tooltip" data-placement="top" title="Lock">
+					<span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+				  </a>
+				  <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+					<span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+				  </a>
+				</div>
+				<!-- /menu footer buttons -->
+			  </div>
+			</div>
 
-                    </ul>
+			<!-- top navigation -->
+			<div class="top_nav">
+			  <div class="nav_menu">
+				  <div class="nav toggle">
+					<a id="menu_toggle"><i class="fa fa-bars"></i></a>
+				  </div>
+				  <nav class="nav navbar-nav">
+				  <ul class=" navbar-right">
+					<li class="nav-item dropdown open" style="padding-left: 15px;">
+					  <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+						<img src="images/img.jpg" alt="">John Doe
+					  </a>
+					  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item"  href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+					  </div>
+					</li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+					<li role="presentation" class="nav-item dropdown open">
+					  <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
+						<i class="fa fa-envelope-o"></i>
+						<span class="badge bg-green">6</span>
+					  </a>
+					  <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
+						<li class="nav-item">
+						  <a class="dropdown-item">
+							<span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+							<span>
+							  <span>John Smith</span>
+							  <span class="time">3 mins ago</span>
+							</span>
+							<span class="message">
+							  Film festivals used to be do-or-die moments for movie makers. They were where...
+							</span>
+						  </a>
+						</li>
+    
+						
+						 
+						</li>
+					  </ul>
+					</li>
+				  </ul>
+				</nav>
+			  </div>
+			</div>
+	
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->names.' '.Auth::user()->lastnames }}
-                                </a>
+			<!-- page content -->
+			<div class="right_col" role="main">
+			  <!-- top tiles -->
+			  <div class="row" style="display: inline-block;" >
+			   @yield('body')
+			</div>
+			</div>
+			
+			<footer>
+			  <div class="pull-right">
+				Ingenieria del Software II  <a href="https://autonoma.edu.co">UAM</a>
+			  </div>
+			  <div class="clearfix"></div>
+			</footer>
+			<!-- /footer content -->
+		  </div>
+		</div>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
-</html>
+		<!-- jQuery -->
+		<script src="{{asset('assets/dashboard/vendors/jquery/dist/jquery.min.js')}}"></script>
+		<!-- Bootstrap -->
+		<script src="{{asset('assets/dashboard/vendors/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+		
+		
+		<!-- Custom Theme Scripts -->
+		<script src="{{asset('assets/dashboard/build/js/custom.min.js')}}"></script>
+		
+	  </body>
+	</html>
--- DateManager/routes/web.php
+++ DateManager/routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('Preuabs route');
-});
-
+Route::get('/', [App\Http\Controllers\LandingController::class, 'index']);
 Auth::routes(['verify'=>true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
