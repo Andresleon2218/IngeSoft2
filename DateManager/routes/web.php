de7ba4d524e6ff965ef72c107a2d3ec2bd1abbe6
andresleon2218 <andresleon643@gmail.com>
2020-12-02 09:04:02 -0500

    Preuabs route

--- DateManager/routes/web.php
+++ DateManager/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Bienvenido');
+    return view('Preuabs route');
 });
 
 Auth::routes(['verify'=>true]);
