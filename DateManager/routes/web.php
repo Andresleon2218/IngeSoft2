a5b072b7544dae8d2c6dc4fe97b05b158e3ad707
andresleon2218 <andresleon643@gmail.com>
2020-12-06 16:59:55 -0500

    Se arreglo las ruta del pdf

--- DateManager/routes/web.php
+++ DateManager/routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\PdfController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,13 +22,14 @@ use App\Http\Controllers\ScheduleController;
 Route::get('/',[LandingController::class, 'index']);
 Auth::routes(['verify'=>true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('dashboard/downloadClientspdf', PdfController::class, 'downloadClient');
-Route::get('dashboard/downloadProspdf', PdfController::class, 'downloadPro');
+ 
+Route::get('dashboard/downloadClientspdf', [PdfController::class, 'downloadClient']);
+Route::get('dashboard/downloadProspdf', [PdfController::class, 'downloadPro']);
+Route::get('dashboard/streamClientspdf', [PdfController::class, 'streamClient']);
+Route::get('dashboard/streamProspdf', [PdfController::class, 'streamPro']);
 
-Route::get('dashboard/streamClientspdf', PdfController::class, 'streamClient');
-Route::get('dashboard/streamProspdf', PdfController::class, 'streamPro');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('/dates',DateController::class)->names('dates');
